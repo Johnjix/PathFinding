42aee1c9a98421be66486bea4d900b89f35471bc
John Jix <John_jix@hotmail.co.uk>
2021-11-01 23:46:20 +0000

    Added shortest path animation

--- src/app/pf-visualiser/pf-visualiser.component.ts
+++ src/app/pf-visualiser/pf-visualiser.component.ts
@@ -18,6 +18,7 @@ export class PfVisualiserComponent implements OnInit {
   cellIdStr: string[] = [];
   startingCell: CellCoordinates;
   endingCell: CellCoordinates;
+  path: cellState[];
 
   constructor() {
     this.gridRow = [];
@@ -46,6 +47,7 @@ export class PfVisualiserComponent implements OnInit {
       distanceFromStart: Infinity,
       previousCell: null,
     };
+    this.path = [];
   }
 
   ngOnInit() {
@@ -115,12 +117,179 @@ export class PfVisualiserComponent implements OnInit {
     let cellId: number = x + this.maxCol * y;
     if (this.selectedCell.start && this.selectedCell.cellId != cellId) {
       this.CELLS[x + this.maxCol * y].start = true;
+      this.CELLS[x + this.maxCol * y].distanceFromStart = 0;
 
       this.selectedCell.start = false;
+      this.selectedCell.distanceFromStart = Infinity;
     } else if (this.selectedCell.end && this.selectedCell.cellId != cellId) {
       this.CELLS[x + this.maxCol * y].end = true;
 
       this.selectedCell.end = false;
     }
   }
+
+  visualiseDijkstra(): void {
+    let unvisited: cellState[] = this.CELLS.filter((c) => !c.visited);
+    let endCellId: number = this.CELLS.findIndex((c) => c.end);
+    let currentCell: cellState | undefined = unvisited
+      .sort((a, b) => a.distanceFromStart - b.distanceFromStart)
+      .shift();
+    this.path.push(this.CELLS[endCellId]);
+
+    let interval = setInterval(() => {
+      // console.log("unvisited", unvisited);
+      // Sort unvisited
+
+      // console.log("currentNode", currentCell);
+
+      if (currentCell) {
+        // Top neighbor
+        this.neighborCellUpdate(currentCell, unvisited, 0, -1);
+
+        // Bottom neighbor
+        this.neighborCellUpdate(currentCell, unvisited, 0, 1);
+
+        // Left neighbor
+        this.neighborCellUpdate(currentCell, unvisited, -1, 0);
+
+        // Right neighbor
+        this.neighborCellUpdate(currentCell, unvisited, 1, 0);
+
+        // Set current cell to visited
+        currentCell.visited = true;
+      }
+      // Update current cell
+      currentCell = unvisited
+        .sort((a, b) => a.distanceFromStart - b.distanceFromStart)
+        .shift();
+
+      // Clear interval
+      if (this.CELLS[endCellId].visited) {
+        this.tracePath();
+        clearInterval(interval);
+      }
+    }, 5);
+  }
+
+  coordToCellId(
+    xCoord: number | undefined,
+    yCoord: number | undefined
+  ): number | null {
+    if (
+      xCoord == undefined ||
+      yCoord == undefined ||
+      xCoord < 0 ||
+      yCoord < 0 ||
+      xCoord > this.maxCol - 1 ||
+      yCoord > this.maxRow - 1
+    ) {
+      return null;
+    } else {
+      return xCoord + this.maxCol * yCoord;
+    }
+  }
+
+  neighborCellUpdate(
+    currentCell: cellState,
+    unvisited: cellState[],
+    xCoordAdjust: number,
+    yCoordAdjust: number
+  ): void {
+    let neighborCellId: number | null = this.coordToCellId(
+      currentCell.xCoord + xCoordAdjust,
+      currentCell.yCoord + yCoordAdjust
+    );
+    if (
+      neighborCellId != null &&
+      unvisited.includes(this.CELLS[neighborCellId])
+    ) {
+      let neighor: cellState = this.CELLS[neighborCellId];
+
+      // Adjust distance from start
+      let altDistance: number = currentCell.distanceFromStart + 1;
+
+      // Update distance again if shorter one is found
+      if (altDistance < neighor.distanceFromStart) {
+        neighor.distanceFromStart = altDistance;
+
+        // Update previous cell to track for path
+        neighor.previousCell = currentCell.cellId;
+      }
+    }
+  }
+
+  tracePath(): void {
+    for (let i = 0; i < this.CELLS.length; i++) {
+      let nextCellId: number = this.CELLS.findIndex((c) => {
+        if (this.path.length - 1 < i) {
+          return false;
+        }
+        return c.cellId == this.path[0 + i].previousCell;
+      });
+
+      if (nextCellId >= 0) {
+        this.path.push(this.CELLS[nextCellId]);
+      } else {
+        break;
+      }
+    }
+    console.log("path", this.path);
+
+    // Reverse path
+    this.path.reverse();
+    let index: number = 0;
+    let interval = setInterval(() => {
+      this.path[index].path = true;
+
+      index++;
+      if (index == this.path.length) {
+        clearInterval(interval);
+      }
+    }, 50);
+  }
+
+  resetGrid(): void {
+    this.path = [];
+
+    this.CELLS.forEach((cell, index) => {
+      this.CELLS[index].visited = false;
+      this.CELLS[index].distanceFromStart = Infinity;
+      this.CELLS[index].start = false;
+      this.CELLS[index].end = false;
+      this.CELLS[index].path = false;
+      this.CELLS[index].previousCell = null;
+    });
+
+    this.setStartEnd(this.startingCell, this.endingCell);
+  }
 }
+
+// if (currentCell) {
+//   // Top neighbor
+//   let topNeighborCellId: number | null = this.coordToCellId(
+//     currentCell.xCoord,
+//     currentCell.yCoord + 1
+//   );
+//   if (
+//     topNeighborCellId &&
+//     unvisited.includes(this.CELLS[topNeighborCellId])
+//   ) {
+//     let topNeighor: cellState = this.CELLS[topNeighborCellId];
+
+//     // Adjust distance from start
+//     let altDistance: number = currentCell.distanceFromStart + 1;
+
+//     // Update distance again if shorter one is found
+//     if (altDistance < topNeighor.distanceFromStart) {
+//       topNeighor.distanceFromStart = altDistance;
+
+//       // Update previous cell to track for path
+//       topNeighor.previousCell = currentCell.cellId;
+//     }
+//   }
+//   // Bottom neighbor
+
+//   // Left neighbor
+
+//   // Right neighbor
+// }
